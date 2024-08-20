7853d3de7b7c105d43450b7a03c99bee1b33cb47
Foysal Ahmmed Limon <afaysal410@gmail.com>
2024-08-20 17:56:27 +0600

    added WindowHandling class

---
+++ src/test/java/org/example/WindowHandling.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import com.microsoft.playwright.*;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class WindowHandling {
+    protected String url = "https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php";
+
+    Playwright playwright;
+    BrowserType browserType;
+    protected Browser browser;
+    protected BrowserContext browserContext;
+    protected Page page;
+
+    @BeforeSuite
+    public void startChromeBrowser() {
+        playwright = Playwright.create();
+        browserType = playwright.chromium();
+        browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browserContext = browser.newContext(new Browser.NewContextOptions());
+
+        page = browser.newPage();
+        System.out.println("Browser version: " + browser.version());
+    }
+
+    @Test
+    public void createNewWindow() throws InterruptedException {
+        page.navigate(url);
+        Thread.sleep(3000);
+
+        Page newWindow = browserContext.newPage();
+        newWindow.navigate("https://practice-automation.com/iframes/");
+        Thread.sleep(3000);
+
+//        switch to first window
+        page.bringToFront();
+        Thread.sleep(3000);
+
+//        switch to new window
+        newWindow.bringToFront();
+        Thread.sleep(3000);
+    }
+
+    @AfterSuite
+    public void closeChromeBrowser() {
+        page.close();
+        browser.close();
+        playwright.close();
+    }
+}
